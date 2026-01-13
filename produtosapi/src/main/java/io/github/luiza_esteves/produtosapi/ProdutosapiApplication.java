4432f13b077de33597956ed213936de5572710e4
Luíza <luizaesteves987@gmail.com>
2026-01-12 22:16:48 -0300

    hello world endpoint

--- produtosapi/src/main/java/io/github/luiza_esteves/produtosapi/ProdutosapiApplication.java
+++ produtosapi/src/main/java/io/github/luiza_esteves/produtosapi/ProdutosapiApplication.java
@@ -2,9 +2,18 @@ package io.github.luiza_esteves.produtosapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ProdutosapiApplication {
+	//http://localhost:8080/hello-world -> porta 8080 é a padrao da tomcat
+	// mapeando o retorno do metodo para uma chamada get
+	@GetMapping("/hello-world")
+	public String helloWorld(){
+		return "Hello World";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProdutosapiApplication.class, args);
