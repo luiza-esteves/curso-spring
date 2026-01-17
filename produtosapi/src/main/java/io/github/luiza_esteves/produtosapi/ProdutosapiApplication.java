5f097687d19aae05cacd16606ba62a590fcd0b3c
Luíza Esteves <luizaesteves987@gmail.com>
2026-01-17 15:41:34 -0300

    Revert "feat: finalizando endpoint e requisicoes iniciais no banco"
    This reverts commit 7427eacc9a40e1ad47e3758c3808c0838fa4bf0f.

---
+++ produtosapi/src/main/java/io/github/luiza_esteves/produtosapi/ProdutosapiApplication.java
@@ -0,0 +1,16 @@
+package io.github.luiza_esteves.produtosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ProdutosapiApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutosapiApplication.class, args);
+	}
+
+}
